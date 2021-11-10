fac67813ed21e1b7e8bd0b274046297ece4dd757
Lordi <tobias.m.stenger@gmail.com>
2021-11-10 19:26:18 +0100

    base

---
+++ src/main/java/tech/stenger/toolinventory/ToolinventoryApplication.java
@@ -0,0 +1,13 @@
+package tech.stenger.toolinventory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToolinventoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToolinventoryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/tech/stenger/toolinventory/ToolinventoryApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.stenger.toolinventory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToolinventoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
